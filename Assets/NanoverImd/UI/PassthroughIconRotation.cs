38419e77d3f052f6ef985da3e1d55ffdc247ff52
mark wonnacott <ragzouken@gmail.com>
2026-06-15 12:32:54 +0200

    mysterious error

--- Assets/NanoverImd/UI/PassthroughIconRotation.cs
+++ Assets/NanoverImd/UI/PassthroughIconRotation.cs
@@ -1,39 +1,41 @@
 using System;
-using NanoverImd;
 using UnityEngine;
 
-public class PassthroughIconRotation : MonoBehaviour
+namespace NanoverImd.UI
 {
-
-    [SerializeField]
-    private UnityEngine.UI.Image buttonIcon;
-
-    [SerializeField]
-    private NanoverImdApplication nanoverApp;
-
-
-    [SerializeField]
-    private Sprite[] sprites;
-
-    private void Start()
+    public class PassthroughIconRotation : MonoBehaviour
     {
-        buttonIcon = this.gameObject.GetComponent<UnityEngine.UI.Image>();
-        if (buttonIcon == null)
+
+        [SerializeField]
+        private UnityEngine.UI.Image buttonIcon;
+
+        [SerializeField]
+        private NanoverImdApplication nanoverApp;
+
+
+        [SerializeField]
+        private Sprite[] sprites;
+
+        private void Start()
         {
-            Debug.LogError("PassthroughIconRotation: No Image component found on the GameObject.");
-            return;
+            buttonIcon = this.gameObject.GetComponent<UnityEngine.UI.Image>();
+            if (buttonIcon == null)
+            {
+                Debug.LogError("PassthroughIconRotation: No Image component found on the GameObject.");
+                return;
+            }
+
+            UpdatePassthroughIcon();
         }
 
-        UpdatePassthroughIcon();
+        // From a percentage 0.0 to 1.0, update the button icon using the loaded sprites
+        public void UpdatePassthroughIcon()
+        {
+            float passthroughPercentage = nanoverApp.GetPassthroughValue();
+            int spriteIndex = Mathf.Clamp(Mathf.FloorToInt(passthroughPercentage * sprites.Length), 0, sprites.Length - 1);
+            buttonIcon.sprite = sprites[spriteIndex];
+        }
+
+
     }
-
-    // From a percentage 0.0 to 1.0, update the button icon using the loaded sprites
-    public void UpdatePassthroughIcon()
-    {
-        float passthroughPercentage = nanoverApp.GetPassthroughValue();
-        int spriteIndex = Mathf.Clamp(Mathf.FloorToInt(passthroughPercentage * sprites.Length), 0, sprites.Length - 1);
-        buttonIcon.sprite = sprites[spriteIndex];
-    }
-
-
 }
