7d9a0f2cbd95869651ee4e649fc06e96c664b1fa
mark wonnacott <ragzouken@gmail.com>
2026-07-09 13:26:12 +0200

    slider + commands support

--- Assets/NanoverImd/UI/Adhoc Panels/AdhocElement.cs
+++ Assets/NanoverImd/UI/Adhoc Panels/AdhocElement.cs
@@ -10,6 +10,9 @@ namespace NanoverImd.UI
     {
         [Header("Setup")]
         [SerializeField]
+        private NanoverImdApplication app;
+        
+        [SerializeField]
         private Text headerElement;
 
         [SerializeField]
@@ -17,17 +20,43 @@ namespace NanoverImd.UI
 
         [SerializeField]
         private GameObject sliderElement;
+        // hack
+        [SerializeField]
+        private UiButton sliderButtonLeft;
+        [SerializeField]
+        private UiButton sliderButtonRight;
+
+        private AdhocElementData elementData;
+
+        private void Awake()
+        {
+            buttonElement.OnClick += OnButtonClick;
+            sliderButtonLeft.OnClick += OnSliderLeft;
+            sliderButtonRight.OnClick += OnSliderRight;
+        }
+
+        private T SetData<T>(object data) where T : AdhocElementData
+        {
+            var converted = Serialization.FromDataStructure<T>(data);
+            elementData = converted;
+            return converted;
+        }
+
+        private T GetData<T>() where T : AdhocElementData
+        {
+            return elementData as T;
+        }
 
         public void Configure(AdhocElementData data)
         {
             Clear();
 
             if (data.Type == "header") { 
-                ConfigureAsHeader(Serialization.FromDataStructure<AdhocHeaderData>(data.Other));
+                ConfigureAsHeader(SetData<AdhocHeaderData>(data.Other));
             } else if (data.Type == "button") { 
-                ConfigureAsButton(Serialization.FromDataStructure<AdhocButtonData>(data.Other));
+                ConfigureAsButton(SetData<AdhocButtonData>(data.Other));
             } else if (data.Type == "slider") {
-                ConfigureAsSlider(Serialization.FromDataStructure<AdhocSliderData>(data.Other));
+                ConfigureAsSlider(SetData<AdhocSliderData>(data.Other));
             }
         }
 
@@ -50,9 +79,83 @@ namespace NanoverImd.UI
             buttonElement.Text = data.Label;
         }
 
+        public void OnButtonClick()
+        {
+            var data = GetData<AdhocButtonData>();
+            var arguments = new Dictionary<string, object>();
+
+            foreach (var pair in data.Arguments)
+            {
+                if (pair.Value.Literal is { } value)
+                    arguments[pair.Key] = value;
+                else if (pair.Value.Variable is { } variable)
+                    arguments[pair.Key] = app.Simulation.Multiplayer.GetSharedState(variable);
+            }
+
+            app.Simulation.RunCommand(data.Command, arguments);
+        }
+
+        private float GetSliderValue()
+        {
+            var data = GetData<AdhocSliderData>();
+            var value = Mathf.Lerp(data.Range[0], data.Range[1], .5f);
+
+            if (app.Simulation.Multiplayer.GetSharedState(data.Variable) is { } remote)
+            {
+                switch (remote)
+                {
+                    case float number:
+                        value = number;
+                        break;
+                    case int number:
+                        value = number;
+                        break;
+                    case double number:
+                        value = (float) number;
+                        break;
+                }
+            }
+
+            if (data.IsInteger)
+                return Mathf.Round(value);
+
+            return value;
+        }
+
+        private float GetSliderStep()
+        {
+            var data = GetData<AdhocSliderData>();
+
+            if (data.StepSize is { } step)
+                return step;
+
+            if (data.IsInteger)
+                return 1;
+
+            return (data.Range[1] - data.Range[0]) * .1f;
+        }
+
         public void ConfigureAsSlider(AdhocSliderData data)
         {
+            var value = GetSliderValue();
+            var valueText = data.IsInteger ? $"{(int) value}" : $"{value:.2f}";
+
             sliderElement.gameObject.SetActive(true);
+            sliderElement.GetComponentInChildren<Text>().text = $"{data.Label}: {valueText} ({data.Range[0]} - {data.Range[1]})";
+        }
+        
+        private void OnSliderLeft()
+        {
+            var data = GetData<AdhocSliderData>();
+            var value = Mathf.Max(data.Range[0], GetSliderValue() - GetSliderStep());
+            app.Simulation.Multiplayer.SetSharedState(data.Variable, value);
+        }
+
+        private void OnSliderRight()
+        {
+            var data = GetData<AdhocSliderData>();
+            var value = Mathf.Min(data.Range[1], GetSliderValue() + GetSliderStep());
+            app.Simulation.Multiplayer.SetSharedState(data.Variable, value);
         }
     }
 }
--- Assets/NanoverImd/UI/Adhoc Panels/AdhocPanel.cs
+++ Assets/NanoverImd/UI/Adhoc Panels/AdhocPanel.cs
@@ -14,6 +14,9 @@ namespace NanoverImd.UI
         [Header("Setup")]
         [SerializeField]
         private NanoverImdApplication app;
+
+        [SerializeField]
+        private AdhocPanelsManager manager;
         
         [SerializeField]
         private Text titleText;
@@ -30,19 +33,42 @@ namespace NanoverImd.UI
                 transform => transform.gameObject.SetActive(true),
                 transform => transform.gameObject.SetActive(false)
             );
+
+            manager.RegisterPanel(this);
         }
 
-        private void Start()
+        private void OnDestroy()
+        {
+            manager.UnregisterPanel(this);
+        }
+
+        private void OnEnable()
+        {
+            TestConfigure();
+        }
+
+        private void TestConfigure()
         {
             var data = app.Simulation.Multiplayer.SharedStateDictionary.GetValueOrDefault<Dictionary<string, object>>("panel.test");
             if (data != null)
                 Configure(Serialization.FromDataStructure<AdhocPanelData>(data));
         }
 
+        public void Configure()
+        {
+            titleText.text = "No Panel";
+            elementPool.SetActiveInstanceCount(0);
+        }
+
         public void Configure(AdhocPanelData data)
         {
             titleText.text = data.Label ?? "Unnamed Panel";
             elementPool.MapConfig(data.Elements, (data, element) => element.Configure(data));
+        }
+
+        public void OnVariableUpdated(string key, object value)
+        {
+            TestConfigure();
         }
     }
 
@@ -76,14 +102,42 @@ namespace NanoverImd.UI
     [DataContract]
     public class AdhocButtonData : AdhocElementData
     {
-        [DataMember(Name="label")]
+        [DataMember(Name = "label")]
         public string Label { get; set; }
+
+        [DataMember(Name = "command")]
+        public string Command { get; set; }
+
+        [DataMember(Name = "arguments")]
+        public Dictionary<string, ArgumentData> Arguments { get; set; }
+
+        [DataContract]
+        public class ArgumentData
+        {
+            [DataMember(Name = "variable")]
+            public string Variable { get; set; }
+
+            [DataMember(Name = "literal")]
+            public object Literal { get; set; }
+        }
     }
 
     [DataContract]
     public class AdhocSliderData : AdhocElementData
     {
-        [DataMember(Name="label")]
+        [DataMember(Name = "label")]
         public string Label { get; set; }
+
+        [DataMember(Name = "variable")]
+        public string Variable { get; set; }
+
+        [DataMember(Name = "range")]
+        public List<float> Range { get; set; }
+
+        [DataMember(Name = "integer")]
+        public bool IsInteger { get; set; }
+
+        [DataMember(Name = "step")]
+        public float? StepSize { get; set; }
     }
 }
